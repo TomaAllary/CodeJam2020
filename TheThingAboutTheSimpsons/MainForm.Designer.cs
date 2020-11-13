99aa180a1e268a8c068833587d6a324999ec9b4a
manumagre <manu.magre@uqtr.ca>
2020-11-13 23:23:14 +0100

    ui MainForm
    interface graphique du MainForm

--- TheThingAboutTheSimpsons/MainForm.Designer.cs
+++ TheThingAboutTheSimpsons/MainForm.Designer.cs
@@ -23,20 +23,22 @@
         /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.TitleLb = new System.Windows.Forms.Label();
             this.textInput = new System.Windows.Forms.TextBox();
             this.submitBtn = new System.Windows.Forms.Button();
-            this.textOuput = new System.Windows.Forms.TextBox();
-            this.temoin = new System.Windows.Forms.Button();
             this.aboutUsBtn = new System.Windows.Forms.Label();
             this.ourMissionBtn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TitleLb
             // 
             this.TitleLb.AutoSize = true;
+            this.TitleLb.BackColor = System.Drawing.Color.Transparent;
             this.TitleLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TitleLb.Location = new System.Drawing.Point(256, 49);
+            this.TitleLb.ForeColor = System.Drawing.Color.DarkOrange;
+            this.TitleLb.Location = new System.Drawing.Point(262, 75);
             this.TitleLb.Name = "TitleLb";
             this.TitleLb.Size = new System.Drawing.Size(450, 63);
             this.TitleLb.TabIndex = 0;
@@ -44,70 +46,73 @@
             // 
             // textInput
             // 
-            this.textInput.Location = new System.Drawing.Point(411, 184);
+            this.textInput.BackColor = System.Drawing.SystemColors.Window;
+            this.textInput.Location = new System.Drawing.Point(347, 153);
+            this.textInput.Multiline = true;
             this.textInput.Name = "textInput";
-            this.textInput.Size = new System.Drawing.Size(100, 20);
+            this.textInput.Size = new System.Drawing.Size(259, 130);
             this.textInput.TabIndex = 1;
             // 
             // submitBtn
             // 
-            this.submitBtn.Location = new System.Drawing.Point(424, 210);
+            this.submitBtn.Location = new System.Drawing.Point(347, 289);
             this.submitBtn.Name = "submitBtn";
-            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.Size = new System.Drawing.Size(259, 23);
             this.submitBtn.TabIndex = 2;
-            this.submitBtn.Text = "button1";
+            this.submitBtn.Text = "search";
             this.submitBtn.UseVisualStyleBackColor = true;
             this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
-            // 
-            // textOuput
-            // 
-            this.textOuput.Location = new System.Drawing.Point(679, 184);
-            this.textOuput.Name = "textOuput";
-            this.textOuput.Size = new System.Drawing.Size(100, 20);
-            this.textOuput.TabIndex = 3;
-            // 
-            // temoin
-            // 
-            this.temoin.Location = new System.Drawing.Point(704, 141);
-            this.temoin.Name = "temoin";
-            this.temoin.Size = new System.Drawing.Size(75, 23);
-            this.temoin.TabIndex = 5;
-            this.temoin.Text = "button3";
-            this.temoin.UseVisualStyleBackColor = true;
             // 
             // aboutUsBtn
             // 
             this.aboutUsBtn.AutoSize = true;
-            this.aboutUsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.aboutUsBtn.Location = new System.Drawing.Point(450, 457);
+            this.aboutUsBtn.BackColor = System.Drawing.Color.Transparent;
+            this.aboutUsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutUsBtn.ForeColor = System.Drawing.Color.DarkOrange;
+            this.aboutUsBtn.Location = new System.Drawing.Point(12, 548);
             this.aboutUsBtn.Name = "aboutUsBtn";
-            this.aboutUsBtn.Size = new System.Drawing.Size(53, 13);
+            this.aboutUsBtn.Size = new System.Drawing.Size(68, 17);
             this.aboutUsBtn.TabIndex = 7;
             this.aboutUsBtn.Text = "About US";
             // 
             // ourMissionBtn
             // 
             this.ourMissionBtn.AutoSize = true;
-            this.ourMissionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ourMissionBtn.Location = new System.Drawing.Point(541, 457);
+            this.ourMissionBtn.BackColor = System.Drawing.Color.Transparent;
+            this.ourMissionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ourMissionBtn.ForeColor = System.Drawing.Color.DarkOrange;
+            this.ourMissionBtn.Location = new System.Drawing.Point(858, 548);
             this.ourMissionBtn.Name = "ourMissionBtn";
-            this.ourMissionBtn.Size = new System.Drawing.Size(62, 13);
+            this.ourMissionBtn.Size = new System.Drawing.Size(83, 17);
             this.ourMissionBtn.TabIndex = 8;
             this.ourMissionBtn.Text = "Our Mission";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(391, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "No similar episode";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gold;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(953, 581);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ourMissionBtn);
             this.Controls.Add(this.aboutUsBtn);
-            this.Controls.Add(this.temoin);
-            this.Controls.Add(this.textOuput);
             this.Controls.Add(this.submitBtn);
             this.Controls.Add(this.textInput);
             this.Controls.Add(this.TitleLb);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -121,10 +126,9 @@
         private System.Windows.Forms.Label TitleLb;
         private System.Windows.Forms.TextBox textInput;
         private System.Windows.Forms.Button submitBtn;
-        private System.Windows.Forms.TextBox textOuput;
-        private System.Windows.Forms.Button temoin;
         private System.Windows.Forms.Label aboutUsBtn;
         private System.Windows.Forms.Label ourMissionBtn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
