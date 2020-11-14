ae05975dcc3724efe2b1d2871da2110acdadc4df
Toma Allary <59578008+TomaAllary@users.noreply.github.com>
2020-11-13 21:54:59 -0500

    Screen prototype in Form1

--- TheThingAboutTheSimpsons/Form1.Designer.cs
+++ TheThingAboutTheSimpsons/Form1.Designer.cs
@@ -23,13 +23,106 @@
         /// le contenu de cette méthode avec l'éditeur de code.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.TitleLb = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.textOuput = new System.Windows.Forms.TextBox();
+            this.temoin = new System.Windows.Forms.Button();
+            this.aboutUsBtn = new System.Windows.Forms.Label();
+            this.ourMissionBtn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TitleLb
+            // 
+            this.TitleLb.AutoSize = true;
+            this.TitleLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLb.Location = new System.Drawing.Point(388, 51);
+            this.TitleLb.Name = "TitleLb";
+            this.TitleLb.Size = new System.Drawing.Size(136, 63);
+            this.TitleLb.TabIndex = 0;
+            this.TitleLb.Text = "Titre";
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(411, 184);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(100, 20);
+            this.textInput.TabIndex = 1;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(424, 210);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 2;
+            this.submitBtn.Text = "button1";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            // 
+            // textOuput
+            // 
+            this.textOuput.Location = new System.Drawing.Point(679, 184);
+            this.textOuput.Name = "textOuput";
+            this.textOuput.Size = new System.Drawing.Size(100, 20);
+            this.textOuput.TabIndex = 3;
+            // 
+            // temoin
+            // 
+            this.temoin.Location = new System.Drawing.Point(704, 141);
+            this.temoin.Name = "temoin";
+            this.temoin.Size = new System.Drawing.Size(75, 23);
+            this.temoin.TabIndex = 5;
+            this.temoin.Text = "button3";
+            this.temoin.UseVisualStyleBackColor = true;
+            // 
+            // aboutUsBtn
+            // 
+            this.aboutUsBtn.AutoSize = true;
+            this.aboutUsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aboutUsBtn.Location = new System.Drawing.Point(450, 457);
+            this.aboutUsBtn.Name = "aboutUsBtn";
+            this.aboutUsBtn.Size = new System.Drawing.Size(53, 13);
+            this.aboutUsBtn.TabIndex = 7;
+            this.aboutUsBtn.Text = "About US";
+            // 
+            // ourMissionBtn
+            // 
+            this.ourMissionBtn.AutoSize = true;
+            this.ourMissionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ourMissionBtn.Location = new System.Drawing.Point(541, 457);
+            this.ourMissionBtn.Name = "ourMissionBtn";
+            this.ourMissionBtn.Size = new System.Drawing.Size(62, 13);
+            this.ourMissionBtn.TabIndex = 8;
+            this.ourMissionBtn.Text = "Our Mission";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(953, 581);
+            this.Controls.Add(this.ourMissionBtn);
+            this.Controls.Add(this.aboutUsBtn);
+            this.Controls.Add(this.temoin);
+            this.Controls.Add(this.textOuput);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.TitleLb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TitleLb;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.TextBox textOuput;
+        private System.Windows.Forms.Button temoin;
+        private System.Windows.Forms.Label aboutUsBtn;
+        private System.Windows.Forms.Label ourMissionBtn;
     }
 }
 
